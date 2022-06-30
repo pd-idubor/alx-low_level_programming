229ce24022750637d6db3d97b3ba11447a980b92
Precious Idubor <preciousidubor@gmail.com>
2022-06-30 15:05:43 -0700

    Prints to the standard error stream

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - ", 35, 1, stderr);
+	fwrite("Dora Korpar, 2015-10-19\n", 24, 1, stderr);
+	return (1);
+}
+
