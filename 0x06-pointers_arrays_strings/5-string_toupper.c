5dd5a68652d4b2bbcd4827bfa0c5668fa5367bf9
Precious Idubor <preciousidubor@gmail.com>
2022-07-14 08:24:39 -0700

    Converting uppercase letters to lowercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - converts lowercase letters to uppercase letters
+ * @s:string
+ * Return: Uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
