db2e1f9b9e4f2c654fc12dbcb5229e84704eb762
Precious Idubor <preciousidubor@gmail.com>
2022-07-15 04:41:01 -0700

    Printing integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * (-1);
+	}
+
+	else
+		i = n;
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
+}
