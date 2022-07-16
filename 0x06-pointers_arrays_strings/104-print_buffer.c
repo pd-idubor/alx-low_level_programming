414695141f192700c3e273efb422c9c8ce1c2d15
Precious Idubor <preciousidubor@gmail.com>
2022-07-15 20:09:56 -0700

    Printing buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,7 +17,7 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + c) >= size)
-				printf(" ");
+				printf("  ");
 			else
 				printf("%02x", *(b + i + c));
 			if ((i % 2) != 0 && i != 0)
