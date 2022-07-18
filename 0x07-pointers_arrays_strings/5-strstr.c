9a967b1646473e844287eac0854162b91e3882ad
Precious Idubor <preciousidubor@gmail.com>
2022-07-18 06:11:33 -0700

    Chessboard printing

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to first needle or NULL if none
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+		if (*haystack == needle[i])
+		while (haystack[i] == needle[i] && needle[i] != '\0')
+		{
+			i++;
+			return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+/**
+ * print_chessboard - Prints the chessboard
+ * @a: array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int r, c;
+
+	for (r = 0; r < 8; r++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[r][c]);
+		}
+	_putchar('\n');
+	}
+}
