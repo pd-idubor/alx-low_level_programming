0a5b3e64e4c1013a83fc7b7d7201288331888da6
Precious Idubor <preciousidubor@gmail.com>
2022-07-20 02:58:13 -0700

    Recursion in power calculation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x ^ y
+ * @x: number
+ * @y: power
+ * Return: result or -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
