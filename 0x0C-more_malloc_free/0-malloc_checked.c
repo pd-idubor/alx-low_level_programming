3b964ab395d7a13176924f373233268c5ec8394a
Precious Idubor <preciousidubor@gmail.com>
2022-07-28 05:03:58 -0700

    Memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,7 +10,6 @@ void *malloc_checked(unsigned int b)
 	void *mem = malloc(b);
 
 	if (mem == NULL)
-	       exit(98);
-
+		exit(98);
 	return (mem);
-}	
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,5 +3,4 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-
 #endif
