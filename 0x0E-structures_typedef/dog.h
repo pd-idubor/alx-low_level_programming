9d9687e27bd7bbc96df08795363bf7c694b3ba9a
Precious Idubor <preciousidubor@gmail.com>
2022-08-01 12:29:04 -0700

    Dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines essentials of a dog
+ * @name: dog name
+ * @age: age
+ * @owner: name of ownet
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
