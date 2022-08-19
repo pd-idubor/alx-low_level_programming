0668b5b6be0526467ed0b56be0f071f9aa4e5f99
Precious Idubor <preciousidubor@gmail.com>
2022-08-18 20:17:42 -0700

    Clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * clear_bit - Sets the value of a bit to 0 at a given index
  * @n: number to be converted
@@ -7,9 +8,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8) || n == NULL)
-		return (-1);
+	unsigned long int m = 1;
 
-	*n &= ~(1 << index);
+	m = m << index;
+	if (index > (sizeof(unsigned long int) * 8) || n == NULL)
+		return (-1);
+	if (((*n >> index) & 1) == 1)
+		*n = m ^ *n;
+
 	return (-1);
 }
